2a6172770ea6825b00a6fd1aa7f9b00cfa9003ac
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-06-01 12:42:51 +0900

    Fix browser router setting

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import Articles from './components/Articles';
 import Article from './components/Article';
@@ -10,19 +10,19 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: '/articles',
-        element: <Articles />
+        path: 'articles',
+        element: <Articles />,
       },
       {
-        path: '/articles/:id',
-        element: <Article />
-      }
-    ]
-  }
-])
+        path: 'articles/:id',
+        element: <Article />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
