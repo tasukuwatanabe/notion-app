e95b4b3f476772edc09f1e7ffeb9fb2f78a610f1
tasukuwatanabe <tasuku.watanabe.325@gmail.com>
2024-06-06 23:51:45 +0900

    Install react-helmet

--- src/layouts/Root.tsx
+++ src/layouts/Root.tsx
@@ -1,9 +1,18 @@
 import { ReactNode } from "react";
+import { Helmet } from "react-helmet";
 
 type RootLayoutProps = {
   children: ReactNode;
 };
 
 export default function RootLayout({ children }: RootLayoutProps) {
-  return <div className="h-full bg-red-300">{children}</div>;
+  return (
+    <div className="h-full bg-red-300">
+      <Helmet>
+        <meta name="robots" content="noindex" />
+        <title>Notion App</title>
+      </Helmet>
+      {children}
+    </div>
+  );
 }
